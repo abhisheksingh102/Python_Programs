74b714863ea4a5112eaad3f087eb5dfc6de7917e
abhisheksingh102 <abhishek.singh_cs17@gla.ac.in>
2019-12-11 14:58:22 +0530

    Practise Examples on function

---
+++ Function.py
@@ -0,0 +1,57 @@
+# Program for even odd using function
+'''
+def evenOdd(x):
+    if(x%2==0):
+        print("Even")
+    else:
+        print("odd")
+evenOdd(int(input("Enter x\n")))
+evenOdd(int(input("Enter x\n")))
+'''''
+
+#
+# def my_function():
+#   print("Hello from a function")
+# my_function()
+
+
+# def my_function(fname):
+#   print(fname + " Refsnes")
+#
+# my_function("25")
+# my_function("Tobias")
+# my_function("Linus")
+
+# def my_function(country="USA"):
+# #   print("I am from " + country)
+# #
+# # my_function("Sweden")
+# # my_function("India")
+# # my_function()
+# # my_function("Brazil")
+
+# def my_function(food):
+#   for x in food:
+#     print(x)
+#
+# fruits = ["apple", "banana", "cherry"]
+#
+# my_function(fruits)
+
+# def my_function(x):
+#   return 5 * x
+#
+# print(my_function(3))
+# print(my_function(5))
+# print(my_function(9))
+
+# def my_function(child3, child2, child1):
+#   print("The youngest child is " + child3)
+#
+# my_function(child1 = "Emanual", child2 = "Tobias", child3 = "Linus")
+
+# def my_function(*kids):
+#   print("The youngest child is " + kids[1])
+#
+# my_function("Emil", "Tobias", "Linus")
+
